0e5647df5ead440876735dcd5eec306c2fbcacb4
alex423 <alex-glebe@mail.ru>
2019-11-30 17:03:26 +0200

    1

--- шифр2.c
+++ шифр2.c
@@ -38,9 +38,13 @@ dowhiletrue :
 
 typedef uint8_t ( * arrp ) [ ] ;
 typedef char ( * strp ) [ ] ;
-  
+
+
+// четыре * четыре = шестнадцать
+# define  shifr_deshi_size  ((size_t)0x10)
+
 static  void  initarr ( arrp  const p , uint8_t const codefree ) {
-  uint8_t * i = & ((*p)[0x10]) ;
+  uint8_t * i = & ((*p)[shifr_deshi_size]) ;
   do {
     --  i ;
     ( * i ) = codefree ;
@@ -56,9 +60,9 @@ static  void  printarr  ( strp const  name , arrp const p , size_t const arrsize
   fputs(u8"]\n",stdout); }
   
 static  void  crypt_decrypt ( arrp const datap , arrp const tablep ,
-  arrp const encrp ) {
-  uint8_t const * id = &((*datap)[4]) ;
-  uint8_t * ied = &((*encrp)[4]) ;
+  arrp const encrp , size_t const data_size ) {
+  uint8_t const * id = &((*datap)[data_size]) ;
+  uint8_t * ied = &((*encrp)[data_size]) ;
   do {
     -- id ;
     --  ied ;
@@ -72,6 +76,113 @@ typedef uint8_t ( * type_raspr4_xp  ) [ ] [ 4 ] ;
 # define  raspr4_12_size  ((uint16_t)495U)
 # define  raspr4_8_size ((uint8_t)70U)
 
+struct  s_raspr4 {
+  
+uint8_t x8 [ raspr4_8_size ] [ 4 ] ;
+uint8_t x12 [ raspr4_12_size ] [ 4 ] ;
+uint8_t x16 [ raspr4_16_size ] [ 4 ] ;
+
+// массив размеров разных распределений
+uint16_t  s [ 4 ] ;
+  
+// массив указателей на разные распределения
+type_raspr4_xp  xp  [ 4 ] ;
+  
+} ;
+
+static  struct  s_raspr4 raspr4  = { .s = { [ 3 ] = raspr4_16_size , [ 2 ] = raspr4_12_size ,
+  [ 1 ] = raspr4_8_size , [ 0 ] = 0 } , .xp = { [ 3 ] = & raspr4.x16 , [ 2 ] = & raspr4.x12 ,
+  [ 1 ] = & raspr4.x8 , [ 0 ] = (void*)0  } } ;
+  
+void  raspr4_init ( void  ) {
+  uint8_t raspri  = 4 ;
+  do {
+    uint8_t raspri4 = raspri * 4 ;
+    --  raspri  ;
+    // raspri = 3 , 2 , 1
+    // raspri4 = 16 , 12 , 8
+    { uint16_t  j = 0 ;
+      type_raspr4_xp ap = raspr4.xp [ raspri ] ;
+      // ap - указатели на разные массивы
+      for(uint8_t i0 = 0 ; i0 < (raspri4 - 3)  ; ++  i0 )
+        for(uint8_t i1 = i0 + 1 ; i1 < (raspri4 - 2)  ; ++  i1 )
+          for(uint8_t i2 = i1 + 1 ; i2 < (raspri4 - 1 ) ; ++  i2 )
+            for(uint8_t i3 = i2 + 1 ; i3 < raspri4  ; ++  i3 ) {
+              ( * ap ) [ j ] [ 0 ] = i0  ;
+              ( * ap ) [ j ] [ 1 ] = i1  ;
+              ( * ap ) [ j ] [ 2 ] = i2  ;
+              ( * ap ) [ j ] [ 3 ] = i3  ; 
+              ++  j ; } }
+  } while ( raspri > 1 ) ; }
+  
+// пароль раскладываем в таблицу шифровки , дешифровки
+  // пароль % 0x10 = 0xa означает, что 0xa это шифрованный код для соли+данных 0x0
+  // пароль делим на 16, остаются 15! вариантов пароля
+  // пароль % 0xf = 0xa это порядковый номер для оставшегося НЕ занятого из 0xff
+  // секретных кодов для соли+данных 0x1  
+void  password_load ( uint32_t  const password_const  , arrp const shifrp , arrp const deship ) {
+  
+  uint8_t const codefree = 0xff ;
+  initarr ( shifrp , codefree )  ;
+  initarr ( deship , codefree )  ;
+  uint32_t password = password_const ;
+  uint16_t  cindex4_16  = password  % raspr4_16_size ;
+  password  /=  raspr4_16_size  ;
+  
+  for ( uint8_t j = 0 ; j < 4 ; ++  j ) {
+    (*shifrp) [ j ] = raspr4.x16 [ cindex4_16 ] [ j ] ;
+    (*deship) [ raspr4.x16 [ cindex4_16 ] [ j ] ] = 0 ; }
+  /*
+      printf(u8"shifr [ 0 ] = 0x%x , shifr [ 1 ] = 0x%x , shifr [ 2 ] = 0x%x , shifr [ 3 ] = 0x%x\n",(unsigned int)(*shifrp) [ 0 ],(unsigned int)(*shifrp) [ 1 ],
+        (unsigned int)(*shifrp) [ 2 ],(unsigned int)(*shifrp) [ 3 ]);
+      printf ( u8"пароль = %x\n",password);
+    */  
+      for ( uint8_t arr_ind = 1 ; arr_ind <= 2 ; ++ arr_ind ) {
+        uint8_t index = 0 ;
+        uint16_t  cindex4_i  = password  % ( raspr4.s [ 3 - arr_ind ] ) ;
+        password  /= ( raspr4.s [ 3 - arr_ind ] ) ;
+        { uint8_t old_index = 0xff ;
+          for ( uint8_t j = 0 ; j < 4 ; ++  j ) {
+            uint8_t new_index = ( * ( raspr4.xp [ 3 - arr_ind ] ) )
+              [ cindex4_i ] [ j ] ;
+            { uint8_t passed = old_index + 1 ;
+              do {
+                if ( (*deship) [ index ] == codefree ) {
+                  if ( passed == new_index ) break ;
+                  ++ passed  ; }
+                ++  index ;
+              } while ( true ) ; }
+            old_index = new_index ;
+            (*shifrp) [ 0x4 * arr_ind + j ] = index ;
+            (*deship) [ index ] = arr_ind ;
+            ++  index ; } }
+      /*
+        printf  (
+          u8"shifr [ %x ] = 0x%x , shifr [ %x ] = 0x%x , shifr [ %x ] = 0x%x , shifr [ %x ] = 0x%x\n",
+          0x4 * arr_ind,(unsigned int)(*shifrp) [ 0x4 * arr_ind ],
+          0x4 * arr_ind+1,(unsigned int)(*shifrp) [ 0x4 * arr_ind+1 ],
+          0x4 * arr_ind+2,(unsigned int)(*shifrp) [ 0x4 * arr_ind +2 ],
+          0x4 * arr_ind+3,(unsigned int)(*shifrp) [ 0x4 * arr_ind+3 ] ) ;
+        printf ( u8"пароль = %x\n",password);*/ }
+      
+      // пароль больше не нужен , беру оставшиеся коды
+  
+      { uint8_t index3 = 0 ;
+        for ( uint8_t  i = 0xc  ; i <=  0xf ; ++  i ) {  
+          do  {
+            if ( (*deship) [ index3 ] == codefree ) break  ;
+            ++  index3 ;
+          } while ( true  ) ; 
+          (*shifrp) [ i ] = index3 ;
+          (*deship) [ index3 ] = 3 ;
+          ++  index3 ; } }
+/*
+      printf(u8"shifr [ c ] = 0x%x , shifr [ d ] = 0x%x , shifr [ e ] = 0x%x , shifr [ f ] = 0x%x\n",(unsigned int)(*shifrp) [ 0xc ],(unsigned int)(*shifrp) [ 0xd ],
+        (unsigned int)(*shifrp) [ 0xe ],(unsigned int)(*shifrp) [ 0xf ]);*/
+  }
+
+# define  alldata_size  12
+  
 int main(){
   setlocale(LC_ALL,"");
   unsigned long const fact4 = fact(4) ;
@@ -87,168 +198,25 @@ int main(){
   uint32_t const password_const  = (  ( long double ) rand  ( ) ) /
     ( ( long double ) RAND_MAX  ) * ( ( long double ) passmax ) ;
   printf(u8"пароль = 0x%x\n",password_const);
-  // пароль раскладываем в таблицу шифровки , дешифровки
-  // пароль % 0x10 = 0xa означает, что 0xa это шифрованный код для соли+данных 0x0
-  // пароль делим на 16, остаются 15! вариантов пароля
-  // пароль % 0xf = 0xa это порядковый номер для оставшегося НЕ занятого из 0xff
-  // секретных кодов для соли+данных 0x1
   
-  // массив размеров разных распределений
-  uint16_t  raspr4_s [ 4 ] = { [ 3 ] = raspr4_16_size , [ 2 ] = raspr4_12_size ,
-    [ 1 ] = raspr4_8_size , [ 0 ] = 0 } ;  
+  raspr4_init ( ) ;
   
-  uint8_t raspr4_16x [ raspr4_16_size ] [ 4 ] ;
-          
-  uint8_t raspr4_12x [ raspr4_12_size ] [ 4 ] ;
-
-  uint8_t raspr4_8x [ raspr4_8_size ] [ 4 ] ;
-                
-  // массив указателей на разные распределения
-  type_raspr4_xp  raspr4_x  [ 4 ] = { [ 3 ] = & raspr4_16x , [ 2 ] = & raspr4_12x ,
-    [ 1 ] = & raspr4_8x , [ 0 ] = (void*)0  } ;
-        
-  { uint8_t raspri  = 4 ;
-    do {
-      uint8_t raspri4 = raspri * 4 ;
-      --  raspri  ;
-      // raspri = 3 , 2 , 1
-      // raspri4 = 16 , 12 , 8
-      { uint16_t  j = 0 ;
-        type_raspr4_xp ap = raspr4_x [ raspri ] ;
-        // ap - указатели на разные массивы
-        for(uint8_t i0 = 0 ; i0 < (raspri4 - 3)  ; ++  i0 )
-          for(uint8_t i1 = i0 + 1 ; i1 < (raspri4 - 2)  ; ++  i1 )
-            for(uint8_t i2 = i1 + 1 ; i2 < (raspri4 - 1 ) ; ++  i2 )
-              for(uint8_t i3 = i2 + 1 ; i3 < raspri4  ; ++  i3 ) {
-                ( * ap ) [ j ] [ 0 ] = i0  ;
-                ( * ap ) [ j ] [ 1 ] = i1  ;
-                ( * ap ) [ j ] [ 2 ] = i2  ;
-                ( * ap ) [ j ] [ 3 ] = i3  ; 
-                ++  j ; } }
-    } while ( raspri > 1 ) ; }
-  
-  uint8_t shifr [ 0x10 ] ;
+  uint8_t shifr [ shifr_deshi_size ] ;
   // 0 .. 3 - варианты секретных кодов для буквы 0
   // 4 .. 7 - варианты секретных кодов для буквы 1
   // 8 .. b - варианты секретных кодов для буквы 2
   // c .. f - варианты секретных кодов для буквы 3
   
-  uint8_t deshi [ 0x10 ] ;
+  uint8_t deshi [ shifr_deshi_size ] ;
   
-  // !!!
-  // ... перекинуть в отдельную функцию : ( password const , & array , & array )
-  
-  uint8_t const codefree = 0xff ;
-  initarr ( & shifr , codefree )  ;
-  initarr ( & deshi , codefree )  ;
-  uint32_t password = password_const ;
-  uint16_t  cindex4_16  = password  % raspr4_16_size ;
-  password  /=  raspr4_16_size  ;
-  
-  for ( uint8_t j = 0 ; j < 4 ; ++  j ) {
-    shifr [ j ] = raspr4_16x [ cindex4_16 ] [ j ] ;
-    deshi [ raspr4_16x [ cindex4_16 ] [ j ] ] = 0 ; }
-  
-      printf(u8"shifr [ 0 ] = 0x%x , shifr [ 1 ] = 0x%x , shifr [ 2 ] = 0x%x , shifr [ 3 ] = 0x%x\n",(unsigned int)shifr [ 0 ],(unsigned int)shifr [ 1 ],
-        (unsigned int)shifr [ 2 ],(unsigned int)shifr [ 3 ]);
-      printf ( u8"пароль = %x\n",password);
-      
-      for ( uint8_t arr_ind = 1 ; arr_ind <= 2 ; ++ arr_ind ) {
-        uint8_t index = 0 ;
-        uint16_t  cindex4_i  = password  % ( raspr4_s [ 3 - arr_ind ] ) ;
-        password  /= ( raspr4_s [ 3 - arr_ind ] ) ;
-        { uint8_t old_index = 0xff ;
-          for ( uint8_t j = 0 ; j < 4 ; ++  j ) {
-            uint8_t new_index = ( * ( raspr4_x [ 3 - arr_ind ] ) )
-              [ cindex4_i ] [ j ] ;
-            { uint8_t passed = old_index + 1 ;
-              do {
-                if ( deshi [ index ] == codefree ) {
-                  if ( passed == new_index ) break ;
-                  ++ passed  ; }
-                ++  index ;
-              } while ( true ) ; }
-            old_index = new_index ;
-            shifr [ 0x4 * arr_ind + j ] = index ;
-            deshi [ index ] = arr_ind ;
-            ++  index ; } }
-      
-        printf  (
-          u8"shifr [ %x ] = 0x%x , shifr [ %x ] = 0x%x , shifr [ %x ] = 0x%x , shifr [ %x ] = 0x%x\n",
-          0x4 * arr_ind,(unsigned int)shifr [ 0x4 * arr_ind ],
-          0x4 * arr_ind+1,(unsigned int)shifr [ 0x4 * arr_ind+1 ],
-          0x4 * arr_ind+2,(unsigned int)shifr [ 0x4 * arr_ind +2 ],
-          0x4 * arr_ind+3,(unsigned int)shifr [ 0x4 * arr_ind+3 ] ) ;
-        printf ( u8"пароль = %x\n",password); }
-      /*
-      uint8_t index = 0 ;
-      uint16_t  cindex4_12  = password  % ( raspr4_s [ 2 ] ) ;
-      password  /= ( raspr4_s [ 2 ] ) ;
-      
-// !!!
-  // ... все циклы собрать компактно
-      
-      { uint8_t old_index = 0xff ;
-        for ( uint8_t j = 0 ; j < 4 ; ++  j ) {
-          uint8_t new_index = ( * ( raspr4_x [ 2 ] ) ) [ cindex4_12 ] [ j ] ;
-          { uint8_t passed = old_index + 1 ;
-            do {
-              if ( deshi [ index ] == codefree ) {
-                if ( passed == new_index ) break ;
-                ++ passed  ; }
-              ++  index ;
-            } while ( true ) ; }
-          old_index = new_index ;
-          shifr [ 0x4 + j ] = index ;
-          deshi [ index ] = 1 ;
-          ++  index ; } }
-      
-      printf(u8"shifr [ 4 ] = 0x%x , shifr [ 5 ] = 0x%x , shifr [ 6 ] = 0x%x , shifr [ 7 ] = 0x%x\n",(unsigned int)shifr [ 4 ],(unsigned int)shifr [ 5 ],
-        (unsigned int)shifr [ 6 ],(unsigned int)shifr [ 7 ]);
-      printf ( u8"пароль = %x\n",password);
-      index = 0 ;
-// беру mod 70 , если пароль очень большой (больше нормы), чтобы получить < 70
-      uint16_t  cindex4_8  = password  % ( raspr4_s [ 1 ] ) ;
-
-      { uint8_t old_index = 0xff ;
-        for ( uint8_t j = 0 ; j < 4 ; ++  j ) {
-          uint8_t new_index = ( * ( raspr4_x [ 1 ] ) ) [ cindex4_8 ] [ j ] ;
-          { uint8_t passed = old_index + 1 ;
-            do {
-              if ( deshi [ index ] == codefree ) {
-                if ( passed == new_index ) break ;
-                ++ passed  ; }
-              ++  index ;
-            } while ( true ) ; }
-          old_index = new_index ;
-          shifr [ 0x8 + j ] = index ;
-          deshi [ index ] = 2 ;
-          ++  index ; } }
-      
-      printf(u8"shifr [ 8 ] = 0x%x , shifr [ 9 ] = 0x%x , shifr [ a ] = 0x%x , shifr [ b ] = 0x%x\n",(unsigned int)shifr [ 8 ],(unsigned int)shifr [ 9 ],
-        (unsigned int)shifr [ 0xa ],(unsigned int)shifr [ 0xb ]);
-*/
-      // пароль больше не нужен , беру оставшиеся коды
-  
-      { uint8_t index3 = 0 ;
-        for ( uint8_t  i = 0xc  ; i <=  0xf ; ++  i ) {  
-          do  {
-            if ( deshi [ index3 ] == codefree ) break  ;
-            ++  index3 ;
-          } while ( true  ) ; 
-          shifr [ i ] = index3 ;
-          deshi [ index3 ] = 3 ;
-          ++  index3 ; } }
-
-      printf(u8"shifr [ c ] = 0x%x , shifr [ d ] = 0x%x , shifr [ e ] = 0x%x , shifr [ f ] = 0x%x\n",(unsigned int)shifr [ 0xc ],(unsigned int)shifr [ 0xd ],
-        (unsigned int)shifr [ 0xe ],(unsigned int)shifr [ 0xf ]);
-       
-  printarr(&u8"таблица шифровать",&shifr,0x10);
-  printarr(&u8"расшифровывать   ",&deshi,0x10);
-  uint8_t secretdata [ 4 ] = { 0 , 1 , 2 , 3 } ;
-  uint8_t secretdatasole  [ 4 ] ;
-  { uint8_t const * id = &(secretdata[4]) ;
-    uint8_t * ids = &(secretdatasole[4]) ;
+  password_load ( password_const  , & shifr , & deshi ) ;
+     
+  printarr(&u8"таблица шифровать",&shifr,shifr_deshi_size);
+  printarr(&u8"расшифровывать   ",&deshi,shifr_deshi_size);
+  uint8_t secretdata [ alldata_size ] = { 0 , 1 , 2 , 3 , 3 , 2 , 1 , 0 , 3 , 0 , 2 , 1 } ;
+  uint8_t secretdatasole  [ alldata_size ] ;
+  { uint8_t const * id = &(secretdata[alldata_size]) ;
+    uint8_t * ids = &(secretdatasole[alldata_size]) ;
     int ran = rand()  ;
     do {
       -- id ;
@@ -258,20 +226,58 @@ int main(){
       (* ids) = ((* id)<<2) bitor (ran%4) ;
       ran >>= 2 ;
     } while ( id not_eq &(secretdata[0]) ) ; }
-  printarr(&u8"секретные данные",&secretdata,4);  
-  printarr(&u8"с солью         ",&secretdatasole,4);
-  uint8_t encrypteddata [ 4 ] ;
-  crypt_decrypt ( & secretdatasole , & shifr , & encrypteddata ) ;
-  printarr(&u8"зашифрованные данные ",&encrypteddata,4);
-  uint8_t decrypteddata [ 4 ] ;
-  crypt_decrypt ( & encrypteddata , & deshi , & decrypteddata ) ;
-  printarr(&u8"расшифрованные данные",&decrypteddata,4);
-  
+  printarr(&u8"секретные данные",&secretdata,alldata_size);  
+  printarr(&u8"с солью         ",&secretdatasole,alldata_size);
+  uint8_t encrypteddata [ alldata_size ] ;
+  crypt_decrypt ( & secretdatasole , & shifr , & encrypteddata , alldata_size ) ;
+  printarr(&u8"зашифрованные данные ",&encrypteddata,alldata_size);
+  uint8_t decrypteddata [ alldata_size ] ;
+  crypt_decrypt ( & encrypteddata , & deshi , & decrypteddata , alldata_size ) ;
+  printarr(&u8"расшифрованные данные",&decrypteddata,alldata_size);
+  size_t  decode_count = 0 ;
+  uint32_t password_try ;
+  uint8_t decrypteddata_try [ alldata_size ] ;
+  uint8_t shifr_try [ shifr_deshi_size ] ;
+  uint8_t deshi_try [ shifr_deshi_size ] ;
   puts(u8"Попытаюсь подобрать...");
-  uint32_t const password_try = (  ( long double ) rand  ( ) ) /
+  bool  flag_equal  ;
+  do {
+  
+  password_try = (  ( long double ) rand  ( ) ) /
     ( ( long double ) RAND_MAX  ) * ( ( long double ) passmax ) ;
-  printf(u8"попытка , пароль = 0x%x\n",password_try);
-  uint8_t decrypteddata_try [ 4 ] = { } ;
-  //crypt_decrypt ( & encrypteddata_try , & deshi_try , & decrypteddata_try ) ;
-  printarr(&u8"попытка , расшифрованные данные",&decrypteddata_try,4);
+  //printf(u8"попытка , пароль = 0x%x\n",password_try);
+  
+  // 0 .. 3 - варианты секретных кодов для буквы 0
+  // 4 .. 7 - варианты секретных кодов для буквы 1
+  // 8 .. b - варианты секретных кодов для буквы 2
+  // c .. f - варианты секретных кодов для буквы 3
+  
+  password_load ( password_try  , & shifr_try , & deshi_try ) ;
+     
+  //printarr(&u8"попытка таблица шифровки   ",&shifr_try,shifr_deshi_size);
+  //printarr(&u8"попытка таблица расшифровки",&deshi_try,shifr_deshi_size);
+  
+  crypt_decrypt ( & encrypteddata , & deshi_try , & decrypteddata_try , alldata_size ) ;
+  //printarr(&u8"попытка , расшифрованные данные",&decrypteddata_try,4);
+  flag_equal  = true ;
+  for(int i = alldata_size ; i ; ) {
+    --  i ;
+    if  ( decrypteddata_try [ i ] not_eq secretdata [ i ] ) {
+      flag_equal = false ;
+      break ; } }
+  ++  decode_count  ;
+ } while ( not flag_equal ) ;
+ printf(u8"количество попыток угадать данные = %ld\n",decode_count);
+ printf(u8"попытка , пароль = 0x%x\n",password_try);
+ printarr(&u8"попытка таблица расшифровки",&deshi_try,shifr_deshi_size); 
+  // на четыре числа сложность угадать :
+  // 355 3 832 206 111 262 138 180 74 45
+  // = 220.6 раз 
+  // на восемь чисел сложность угадать :
+  // 3328 6383 64310 604 9 23976 16087 5167 6162 7161
+  // = 13320 раз
+  // на двенадцать чисел сложность угадать :
+  // 142174 219214 166462 946553 6386 27140 71806 444053 621050 2295
+  // = 
+  // теоретическая сложность = 16! / ((4!)^4) / 2 = 31 531 500
 }
